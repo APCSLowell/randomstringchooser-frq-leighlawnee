ab68119529bc964d547f4c7596e031cb0c244914
leighlawnee <143223104+leighlawnee@users.noreply.github.com>
2024-04-22 11:21:12 -0700

    Update RandomStringChooser.java

--- src/main/java/RandomStringChooser.java
+++ src/main/java/RandomStringChooser.java
@@ -12,12 +12,13 @@ ogString = new ArrayList<String>();
 
 public String getNext(){
 String temp = “”;
-  if(ogString.size()>0){
+  if(ogString.size() == 0)
+    return "NONE";
+  else{
     int j = (int)(Math.random()*ogString.size());
     temp = ogString.get(j);
     ogString.remove(j);
     return temp;
-}else
-    return “NONE”;
+}
 }
 }
